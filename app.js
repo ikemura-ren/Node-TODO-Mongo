263a9eba8c88f4ad857747611e0fa8dc46bf9bcc
ikemura-ren <utip.saikaisite@gmail.com>
2022-04-12 16:53:12 +0900

    データベースとの接続完了

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
-
+const connectDB = require("./db/connect");
 const taskRoute = require("./routes/tasks");
+require("dotenv").config();
 
 const PORT = 5000;
 
 app.use("/api/v1/tasks" ,taskRoute);
 
-app.listen(PORT, console.log("サーバーが起動しました"));
+const start = async () =>{
+    try{
+        await connectDB(process.env.MONGO_URL);
+            app.listen(PORT, console.log("サーバーが起動しました"));
+    }catch(err){
+        console.log(err);
+    }
+}
+
+start();
