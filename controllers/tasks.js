7d0203f457b06d34197b5933535efef4b84e44e3
ikemura-ren <utip.saikaisite@gmail.com>
2022-04-16 20:39:42 +0900

    CRUD機能の作成

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,65 @@
-const getAllTasks = (req, res) =>{
-    res.send("タスク全取得");
-}
-const createTask = (req, res) =>{
-    res.send("タスク新規作成");
-}
+const Task = require("../models/Task");
 
-const getSingleTask = (req, res) =>{
-    res.send("タスク新規作成");
-}
+const getAllTasks = async (req, res) =>{
+    try{
+        const allTask = await Task.find({});
+        res.status(200).json(allTask);
+    }catch(err){
+        res.status(500).json(err);
+    }
+};
 
-const updateTask = (req, res) =>{
-    res.send("タスク更新");
-}
+const createTask = async (req, res) =>{
+    try{
+        const createTask = await Task.create(req.body);
+        res.status(200).json(createTask);
+    }catch(err){
+        res.status(500).json(err);
+    }
+};
 
-const deleteTask = (req, res) =>{
-    res.send("タスク削除");
-}
+const getSingleTask = async (req, res) =>{
+    try{
+        const getSingleTask = await Task.findOne({_id: req.params.id });
+        if(!getSingleTask){
+        return res.status(404).json(`_id:${req.params.id}は存在しません`);
+        }
+        res.status(200).json(getSingleTask);
+    }catch(err){
+        res.status(500).json(err);
+    }
+};
+
+const updateTask = async (req, res) =>{
+    try{
+        const updateTask = await Task.findOneAndUpdate(
+            {_id: req.params.id },
+            req.body,
+            {
+                new: true,
+            }
+            );
+        if(!updateTask){
+        return res.status(404).json(`_id:${req.params.id}は存在しません`);
+        }
+        res.status(200).json(updateTask);
+    }catch(err){
+        res.status(500).json(err);
+    }
+};
+
+const deleteTask =  async (req, res) =>{
+    try{
+        const deleteTask = await Task.findOneAndDelete(
+            {_id: req.params.id },);
+        if(!deleteTask){
+        return res.status(404).json(`_id:${req.params.id}は存在しません`);
+        }
+        res.status(200).json(deleteTask);
+    }catch(err){
+        res.status(500).json(err);
+    }
+};
 
 module.exports = {
     getAllTasks,
