f10533159a1e8142130372dfe0ec460b7bc7e560
ikemura-ren <utip.saikaisite@gmail.com>
2022-04-16 21:50:25 +0900

    public追加

--- app.js
+++ app.js
@@ -3,15 +3,18 @@ const app = express();
 const connectDB = require("./db/connect");
 const taskRoute = require("./routes/tasks");
 require("dotenv").config();
-app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.static("./public/"));
 
 app.use("/api/v1/tasks" ,taskRoute);
 
+
 const start = async () =>{
     try{
-        await connectDB(process.env.MONGO_URL);
+        await connectDB(process.env.MONGO_URL || process.env.MONGODB_URI);
             app.listen(PORT, console.log("サーバーが起動しました"));
     }catch(err){
         console.log(err);
